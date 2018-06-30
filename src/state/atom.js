62a2d784c82c78a9638e6a102bcfdc7e6ae30c1d
Samuel Thomas <samuelfullerthomas@gmail.com>
2018-06-30 15:53:31 +0100

    kartoffel

--- src/state/atom.js
+++ src/state/atom.js
@@ -2,6 +2,6 @@ import createAtom from 'tiny-atom'
 import log from 'tiny-atom/log'
 import actions from './actions'
 
-const initialState = { test: ' hello hello ' }
+const initialState = { test: ' kartoffel kartoffel ' }
 
 module.exports = createAtom(initialState, actions, { debug: log })
